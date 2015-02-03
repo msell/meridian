dc309b15e6dcfddd420394857f6c9802005da722
Matt Sell <mwsell@gmail.com>
2015-02-02 21:44:28 -0600

    writing some tests

--- scrapeSongbook.js
+++ scrapeSongbook.js
@@ -1,68 +1,130 @@
 var cheerio = require('cheerio');
 var fs = require('fs');
 var _ = require('lodash');
-var mongoose = require('mongoose');
-
-var artistSchema = mongoose.Schema({
-    name: String,
-    songs: []
-});
-
-var Artist = mongoose.model('Artist', artistSchema);
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/songbook');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
-  // yay!
-});
-
-console.log('migrating song data');
-var $ = cheerio.load(fs.readFileSync('./songbook.html'), {
-    normalizeWhitespace: false
-});
-
-var exceptionFile = fs.createWriteStream('exception.txt');
-
-var everything = $('#tableBook tr td').get();
-
-var artists = $('b').get();
-var songs = $('i').get();
-
-var expectedTotal = artists.length + songs.length;
-console.log('total rows: ' + everything.length + ' should equal ' + expectedTotal);
+var request = require('superagent-as-promised');
+var RSVP = require('rsvp');
 
 var a = 0;
 var s = 0;
 var transformed = null;
 
-for (var i = 0; i < everything.length; i++) {
+var host = 'http://localhost:1337/';
+var artistUrl = host + 'artist/create';
+var songUrl = host + 'song/create';
+var cursor = 0;
+var $ = null;
 
-    var entry = $(everything).eq(i).text().trim();
+function scrapeData() {
+    $ = cheerio.load(fs.readFileSync('./songbook.html'), {
+        normalizeWhitespace: false
+    });
 
-    if (entry === $(artists).eq(a).text().trim()) {
-        if (transformed != null) {            
-            var artist = new Artist({'name': transformed.artist, 'songs': transformed.songs});
-            artist.save(function(err, artist){
-                
-            });
-            
-        }
+    var everything = $('#tableBook tr td').get();
 
-        transformed = {
-            'artist': entry,
-            'songs': []
-        };
-        a++;
-    } else if (entry === $(songs).eq(s).text().trim()) {
-        transformed.songs.push(entry);
-        s++;
-    } else {
-        console.log('exception no match: ' + entry);
-        exceptionFile.write(entry + '\n');
+    var artists = $('b').get();
+    var songs = $('i').get();
+
+    return {
+        'artists': artists,
+        'songs': songs,
+        'everything': everything
     }
-
 }
 
-exceptionFile.end();
+exports.scrapeData = scrapeData;
+
+function addArtist(name) {
+    console.log('adding %s', name);
+    request.post(artistUrl)
+        .send({
+            'name': name
+        })
+        .end(function (res) {
+              return JSON.parse(res.text).id;            
+        })
+}
+
+function addSong(artistId, name) {
+    console.log('adding %s', name);
+    request.post(songUrl)
+        .send({
+            'name': name,
+            'artist': artistId
+        })
+        .end(function (res) {
+            return res;
+        })
+
+}
+exports.addArtist = addArtist;
+exports.addSong = addSong;
+
+exports.importSongbook = function () {
+    var data = scrapeData();
+
+    for (var i = 0; i < data.everything.length - 1; i++) {
+
+        var entry = $(data.everything).eq(i).text().trim();
+
+        if (entry === $(data.artists).eq(a).text().trim()) {
+            if (transformed != null) {
+                var artist = {
+                    'name': transformed.artist,
+                    'songs': transformed.songs
+                };
+
+                console.log(artist);
+                addArtist(artist.name);
+                //            request.post(artistUrl)
+                //                .send({
+                //                    'name': artist.name
+                //                })
+                //                .then(function (response) {
+                //                    artist.id = JSON.parse(response.text).id;
+                //                    console.log('added artist ' + artist.id);
+                //                    addSongs(artist.id, artist.songs);
+                //                })
+
+
+            }
+
+            transformed = {
+                'artist': entry,
+                'songs': []
+            };
+            a++;
+        } else if (entry === $(data.songs).eq(s).text().trim()) {
+            transformed.songs.push(entry);
+            s++;
+        } else {
+            console.log('exception no match: ' + entry);
+            exceptionFile.write(entry + '\n');
+        }
+    }
+}
+
+
+
+
+
+
+//
+//
+//function addSongs(artistId, songs) {
+//    return new RSVP.Promise(function (resolve, reject) {
+//        console.log(songs);
+//        for (var n = 0; n < songs.length - 1; n++) {
+//            request.post(songUrl)
+//                .send({
+//                    'name': songs[n],
+//                    'artist': artistId
+//                })
+//                .end(function (res) {
+//                    console.log('added song: ' + songs[n]);
+//                    resolve();
+//                })
+//        }
+//    })
+//}
+//
+//exceptionFile.end();
---
+++ test/migration.spec.js
@@ -0,0 +1,25 @@
+var sut = require('../scrapeSongbook.js');
+var should = require('chai').should();
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+describe('scraping the data', function(){
+    it('should fetch songbook data', function(done){
+        var results = sut.scrapeData();
+        results.artists.should.have.length(3);
+        results.everything.should.have.length(17);
+        results.songs.should.have.length(14);
+        done();        
+    });
+})
+
+describe('migrating the songbook',function(){
+    it('should add all the artists', function(done){
+        var spy = sinon.spy(sut.addArtist);
+        sut.importSongbook();
+        spy.callCount.should.equal(3);
+        done();
+    })
+    
+    it('should add all the songs');
+})
