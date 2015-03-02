5b06f51d306571c5ed63b3f937e5d789be67f794
Matt Sell <mwsell@gmail.com>
2015-03-01 18:06:34 -0600

    create update script to add songCount to artist

--- addCount.js
+++ addCount.js
@@ -15,17 +15,30 @@ var cursor = db.song.aggregate([
 //}
 //printjson(foo);
 
-for(var propertyName in cursor){
-    print(propertyName);
-}
+//for(var propertyName in cursor){
+//    print(propertyName);
+//}
 
 for(var propertyName in cursor.result[0]){
     print(propertyName);
 }
 
-print(cursor.result.length);
+print("this many artists: " + cursor.result.length);
+
+//print(cursor.result[2].count);
+
+//printjson(cursor.result[2]);
+
+// TODO:
+// LOOP through groups, update artist where cursor.result[i]._id  .count.
+
 //for(var i=0;i<cursor.result.length;i++){
 //    print(cursor[i].count)
 //}
 
-//for(var i=0;i<)
+for(var i=0;i<cursor.result.length;i++){
+    db.artist.findAndModify({
+        query:{_id: cursor.result[i]._id},
+        update:{$set: {songCount: cursor.result[i].count}}        
+    });
+}
